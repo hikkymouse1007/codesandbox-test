be6fa407d766a363eebed24c41924553787ff5b9
Mashimo Hiroki <54907440+hikkymouse1007@users.noreply.github.com>
2021-04-11 00:18:58 +0900

    Update JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = ""; //input-textの内容を空にする
+
+  // div生成
+  const div = document.createElement("div"); //divタグを変数として宣言
+  div.className = "list-row";
+  // console.log(div);
+
+  //liタグ生成
+  const li = document.createElement("li"); // liタグを変数として宣言
+  li.innerText = inputText;
+  // console.log(li);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // console.log(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+// // target.addEventListener(type, listener [, options]);
